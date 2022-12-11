a5a32eb7eb1de54d143ef3bfeea9a398d5c9538b
Dimas Yudha Pratama <fsdimasyudha@gmail.com>
2022-12-11 17:54:00 +0700

    Add useful info for each command
    Signed-off-by: Dimas Yudha Pratama <fsdimasyudha@gmail.com>

--- personal/android/setup.sh
+++ personal/android/setup.sh
@@ -2,14 +2,21 @@
 #
 # Copyright (C) Dimas Yudha Pratama <fsdimasyudha@gmail.com>
 #
+# This script works flawlessly on Ubuntu 22.04 LTS
+# And proly won't work on below that version
+# Also haven't support yet for other distros
+# But might work on Debian-based distros & other Ubuntu flavors
+#
 read -p 'Input your Git email ~> ' gitEmail
 read -p 'Input your Git name ~> ' gitName
 read -p 'Input your desire number for ccache size in gigs ~> ' ccacheSize
 
 clear
 
+# Enable i386 architecture
 dpkg --add-architecture i386
 
+# Install needed pkgs
 sudo apt-get install -y openjdk-11-jdk \
                         bison \
                         g++-multilib \
@@ -28,19 +35,23 @@ sudo apt-get install -y openjdk-11-jdk \
                         python-is-python3 \
                         zlib1g-dev
 
+# Install repo
 mkdir ~/bin \
 && curl http://commondatastorage.googleapis.com/git-repo-downloads/repo > ~/bin/repo \
 && chmod a+x ~/bin/repo
 
+# Specify user's git email & name
 git config --global user.email "$gitEmail" \
 && git config --global user.name "$gitName"
 
+# Export needed variables & store them all on bashrc
 echo "
 export PATH=~/bin:$PATH
 export USE_CCACHE=1
 export CCACHE_EXEC=/usr/bin/ccache
 " >> ~/.bashrc
 
+# Define ccache size
 ccache -M ${ccacheSize}G
 
 exit 0
